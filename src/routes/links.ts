f481189e949c0ff74bdf651cb14a3fc18ce36e2f
combizera <ycombi349@gmail.com>
2025-05-27 23:30:26 -0300

    feat(link): add delete route

--- src/routes/links.ts
+++ src/routes/links.ts
@@ -59,4 +59,41 @@ export async function linksRoutes(app: FastifyInstance) {
       data: links,
     })
   })
+
+  // DELETE
+  app.delete('/:shortCode', async (request, reply) => {
+    const paramsSchema = z.object({
+      shortCode: z.string().min(3),
+    })
+
+    const result = paramsSchema.safeParse(request.params)
+
+    if (!result.success) {
+      return reply.status(400).send({
+        success: false,
+        error: 'Invalid short code',
+        details: result.error.format(),
+      })
+    }
+
+    const { shortCode } = result.data
+
+    const deleted = await db
+      .delete(linksTable)
+      .where(eq(linksTable.shortCode, shortCode))
+      .returning()
+
+    if (deleted.length === 0) {
+      return reply.status(404).send({
+        success: false,
+        error: 'Short Code not found',
+      })
+    }
+
+    return reply.send({
+      success: true,
+      message: 'Link deleted successfully',
+      data: deleted[0],
+    })
+  })
 }
